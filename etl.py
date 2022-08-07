f53866b7131bb570cde2852f5fca91e9abd6b442
Rafael Carvalho <rafacarvalho301@gmail.com>
2022-08-07 17:06:28 -0300

    feat: Add song data process in etl file

--- etl.py
+++ etl.py
@@ -4,7 +4,7 @@ import os
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import udf, col
 from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, date_format
-
+from pyspark.sql.types import *
 
 config = configparser.ConfigParser()
 config.read('dl.cfg')
@@ -23,22 +23,46 @@ def create_spark_session():
 
 def process_song_data(spark, input_data, output_data):
     # get filepath to song data file
-    song_data = 
+    song_data = f"{input_data}song_data/*/*/*"
     
     # read song data file
-    df = 
+    song_schema = StructType([StructField('artist_id', StringType(), True), \
+                            StructField('artist_latitude', DoubleType(), True), \
+                            StructField('artist_location', StringType(), True), \
+                            StructField('artist_longitude', DoubleType(), True), \
+                            StructField('artist_name', StringType(), True), \
+                            StructField('duration', DoubleType(), True), \
+                            StructField('num_songs', LongType(), True), \
+                            StructField('song_id', StringType(), True), \
+                            StructField('title', StringType(), True), \
+                            StructField('year', IntegerType(), True) \
+                         ])
+    df = spark.read.schema(song_schema).json(song_data)
 
     # extract columns to create songs table
-    songs_table = 
+    cols_songs_table = ['song_id', 'title', 'artist_id', 'year', 'duration']
+    songs_table = df.select(cols_songs_table).dropDuplicates(cols_songs_table)
     
     # write songs table to parquet files partitioned by year and artist
-    songs_table
+    songs_table.write \
+        .option("header", True) \
+        .partitionBy("year", "artist_id") \
+        .mode("Overwrite") \
+        .parquet(f"{output_data}songs_table/")
 
     # extract columns to create artists table
-    artists_table = 
+    cols_artists_table = ['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude']
+    artists_table = df.select(cols_artists_table).dropDuplicates(cols_artists_table)
+    artists_table = artists_table.withColumnRenamed('artist_name', 'name') \
+                                .withColumnRenamed('artist_location', 'location') \
+                                .withColumnRenamed('artist_latitude','latitude') \
+                                .withColumnRenamed('artist_longitude','longitude')
     
     # write artists table to parquet files
-    artists_table
+    artists_table.write \
+        .option("header", True) \
+        .mode("Overwrite") \
+        .parquet(f"{output_data}artists_table/")
 
 
 def process_log_data(spark, input_data, output_data):
@@ -83,8 +107,9 @@ def process_log_data(spark, input_data, output_data):
 
 def main():
     spark = create_spark_session()
-    input_data = "s3a://udacity-dend/"
-    output_data = ""
+    # input_data = "s3a://udacity-dend/"
+    input_data = "s3a://udacity-bkt-rc301-spark-project/data/"
+    output_data = "s3://udacity-bkt-rc301-spark-project/output_data/"
     
     process_song_data(spark, input_data, output_data)    
     process_log_data(spark, input_data, output_data)
